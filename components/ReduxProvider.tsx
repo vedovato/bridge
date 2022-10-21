e5395459ec522958056cb867eb34440cc1371076
andreivedovato1 <andrei.vedovato@opah.com.br>
2022-10-20 23:17:53 -0300

    update code

--- components/ReduxProvider.tsx
+++ components/ReduxProvider.tsx
@@ -2,12 +2,12 @@ import React, { FC } from "react";
 import { Provider } from "react-redux";
 import store from "../store";
 
-interface ReduxProviderProps {
-  // children: JSX.Element | JSX.Element[];
-  children: any;
-}
+// interface ReduxProviderProps {
+//   // children: JSX.Element | JSX.Element[];
+//   children: any;
+// }
 
-const ReduxProvider: FC<ReduxProviderProps> = ({ children }) => {
+const ReduxProvider: FC<any> = ({ children }) => {
   return <Provider store={store}>{children}</Provider>;
 };
 
