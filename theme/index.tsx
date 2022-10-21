56e78acc95648563ddab127d9a7bae25f944be6d
andreivedovato1 <andrei.vedovato@opah.com.br>
2022-10-20 21:27:14 -0300

    add theme

---
+++ theme/index.tsx
@@ -0,0 +1,43 @@
+const theme = {
+  fonts: {
+    body: "Avenir Next",
+    monospace: "monospace",
+  },
+
+  colors: {
+    text: "#000",
+    background: "grey",
+    primary: "purple",
+  },
+
+  text: {
+    h1: {
+      fontSize: 5,
+      fontFamily: "monospace",
+      color: "lime",
+      a: {
+        color: "red",
+      },
+    },
+    thick: {
+      fontWeight: "bold",
+      fontStyle: "italic",
+      fontSize: 3,
+      color: "cyan",
+    },
+  },
+
+  // styles: {
+  //   a: {
+  //     color: 'gold',
+  //   },
+  //   h1: {
+  //     color: 'gold',
+  //   },
+  // },
+};
+
+// export type SaraTheme = typeof theme;
+// const finalTheme: SaraTheme = theme;
+// export default theme as SaraTheme;
+export default theme;
